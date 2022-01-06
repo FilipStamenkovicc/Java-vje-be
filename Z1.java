4abcfce00f27a1a81e3f4a9e154fb589b72ca56d
Filip Stamenković <93558411+FilipStamenkovicc@users.noreply.github.com>
2022-01-06 13:04:51 +0100

    Add files via upload
    Output od 10 stupaca i 10 redova

---
+++ Z1.java
@@ -0,0 +1,20 @@
+package edunova;
+
+public class Z1 {
+
+	public static void main(String[] args) {
+		
+		int [][]a = new int [10][10];
+		
+		for (int i = 0; i <10; i++) {
+			for (int j = 0; j<10; j++) {
+				System.out.printf("%2d ",a[i][j]);;
+			}
+		System.out.println();	
+		}
+		
+		
+	}
+	
+	
+}
